1e0a1b824ed70c7f71f04c3995bb4799694703cf
adig03 <149831123+adig03@users.noreply.github.com>
2025-02-14 11:10:46 +0530

    Initial Commit

---
+++ src/test/java/com/runtimeTerror/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtimeTerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
